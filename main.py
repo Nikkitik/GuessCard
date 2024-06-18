3f9aab3b246110439963cf96b22100050f0f5e34
nikita <nikkitik21@gmail.com>
2024-06-18 18:18:30 +0300

    Исправил проверку ввода пользовательского ответа

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ print()
 cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'T']
 suits = ['♦️', '♥️', '♣️', '♠️']
 
-correct_answers = ['Красная', 'К', 'к', 'Черная', 'Ч', 'ч']
+correct_answers = ['красная', 'к', 'черная', 'ч']
 
 random_card = choice(cards)
 random_suit = choice(suits)
 
 answer = input(
-    'Какой цвет масти загадал бот?\nВведите К(Красная) или Ч(Черная)\n')
+    'Какой цвет масти загадал бот?\nВведите К(Красная) или Ч(Черная)\n').lower()
 
 correct_answer_print = f'Верно! Бот загадал {random_card}{random_suit}'
 wrong_answer_print = f'Неверно! Бот загадал {random_card}{random_suit}'
@@ -24,9 +24,9 @@ while not answer in correct_answers:
         'Введите букву К или Ч.\nВы также можете ввести слово целиком Красная или Черная\n')
     print()
 
-if answer.upper == 'К' and random_suit in ['♦️', '♥️']:
+if answer in ['к', 'красная'] and random_suit in ['♦️', '♥️']:
     print(correct_answer_print)
-elif answer.upper == 'Ч' and random_suit in ['♣️', '♠️']:
+elif answer in ['ч', 'черная'] and random_suit in ['♣️', '♠️']:
     print(correct_answer_print)
 else:
     print(wrong_answer_print)
